d069c11aa19b2d12cd176e2b7038ee3d5c0eb31e
LeilaelRico <A01707023@tec.mx>
2022-08-30 11:42:57 -0500

    Se añade info / Commit inicial de cpp.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Seguridad-Algoritmo-Simetrico
+# TC2007B: Integración de Seguridad Informática en Redes y Sistemas de Software
+
+## Actividad 2: Algoritmo Simétrico
+
+Algoritmo didáctico que incluye operaciones básicas, estas incluyen 2 operaciones de difusión y 2 de confusión.
---
+++ main.cpp
@@ -0,0 +1,11 @@
+/***********************************************************
+ *                      ITESM Querétaro                    *
+ *                          TC2038                         *
+ *          Integración de seguridad informática           *
+ *              en redes y sistemas de software            *
+ *                       Actividad 2                       *
+ *                        01/09/2022                       *
+ *                 Ricardo Núñez Alanís A01703259          *
+ *           Olivia Araceli Morales Quezada A10707371      *
+ *           Cristian Leilael Rico Espinosa A01707023      *
+ ***********************************************************/
